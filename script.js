e5d5ff7ebf4b02a0460a14647525c58b45047b56
szatgabor <gabor.samers@gmail.com>
2021-04-15 19:08:09 +0200

    api issue fixed

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let photosArray = [];
 
 
 //Unsplash API
-const count = 10;
+const count = '10';
 const apiKey = '8I-sepOVnGDuNzGT9BMj6p-Iuv3RDaj0gpqxgEoe-V8';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
